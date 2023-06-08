3c56b59df1df1062d8b121c053dcab385e28a5b6
jaarmitage <130060667+jaarmitage@users.noreply.github.com>
2023-06-08 16:15:30 -0400

    Minor changes, include initial error handler functions.

--- NSX-Certificate-Management/Set-VmwNsxClusterCertificate.ps1
+++ NSX-Certificate-Management/Set-VmwNsxClusterCertificate.ps1
@@ -3,8 +3,11 @@ Function Set-VmwNsxClusterCertificate {
 
     If ($conn -eq $true) {
         Write-Host "Yay!"
+        errHandler 1001
+        FileOpenDialog("D:\Google Drive\Common\Scripts")
+        Write-Host $vmwNsxConnectSpec.Uri
         #Invoke-RestMethod -Url "https://$($connectUri)/api/v1/cluster/api-certificate"
     } Else {
-        Write-Host "Boo!"
+        errHandler 1002
     }
 }
